760470072944d8a62610dcb9693f86906c6f17e7
unomns <unomns@gmail.com>
2025-01-06 11:11:00 +0800

    init commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  toralizer
+//
+//  Created by Murad Nurmukhamedov on 1/6/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
